78bf5a9ab6ffa6a268cae921f6f53d8a1b825d8a
almaz <daniyarovich.z@gmail.com>
2022-10-10 23:20:16 +0600

    Initial Commit

--- start.py
+++ start.py
@@ -4,4 +4,8 @@ os.system("start cmd /k python first.py")
 os.system("start cmd /k python second.py")
 os.system("start cmd /k python thirt.py")
 os.system("start cmd /k python fourth.py")
+os.system("start cmd /k python firsts.py")
+os.system("start cmd /k python seconds.py")
+os.system("start cmd /k python thirts.py")
+os.system("start cmd /k python fourths.py")
 
